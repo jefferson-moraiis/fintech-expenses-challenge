0a6c6bb70fb0b007d099090dab1fddf4828ab328
Jefferson Morais <jefferson.orlando.morais@hotmail.com>
2026-06-17 20:01:14 -0300

    chore: create project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CategoriesModule, TransactionsModule, DashboardModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
