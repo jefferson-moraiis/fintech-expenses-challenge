286705b8a6345cc50fcd8583a2aa87e736fb84ec
Jefferson Morais <jefferson.orlando.morais@hotmail.com>
2026-06-19 13:25:43 -0300

    chore: adjust in shandui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -9,4 +10,9 @@ export default defineConfig({
     tailwindcss(),
     tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+        "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
